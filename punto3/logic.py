38a25759610d04f242bdacef3ce896f562f716ed
Javier Roncallo <jroncallo96@gmail.com>
2018-09-14 00:12:41 -0500

    Punto3 Se crea la case AStar en el modulo logic

--- punto3/logic.py
+++ punto3/logic.py
@@ -5,6 +5,57 @@ def file_to_matrix(path):
     m = [[int(num) for num in line.split(',')] for line in f ]
     return m
 
+class AStar:
+    def __init__(self, start, distance_finder, heuristic):
+        self.start = start
+        self.distance_finder = distance_finder
+        self.heuristic = heuristic
+        
+    def __get_new_current(self, open_set, f_score):
+        lowest = float('inf')
+        new_current = None
+        for node in open_set:
+            if f_score[node] < lowest:
+                lowest = f_score[node]
+                new_current = node
+        return new_current
+
+    def __reconstruct_path(self, parents, current):
+        total_path = [current]
+        for k in parents.keys():
+            current = parents[current]
+            total_path.append(current)
+            if current == self.start: break
+        return total_path
+
+    def evaluate(self, goal):
+        self.heuristic.desired_state = goal
+        # closed_set: the set of nodes already evaluated
+        closed_set = []
+        # open_set: the set of currently discovered nodes that are not evaluated yet
+        open_set = [self.start]
+        parents = {}
+        g_score = {}
+        g_score[self.start] = 0
+        f_score = {}
+        f_score[self.start] = self.heuristic.estimate(self.distance_finder, self.start)
+        while open_set:
+            current = self.__get_new_current(open_set, f_score)
+            if current == goal:
+                return self.__reconstruct_path(parents, current)
+            open_set.remove(current)
+            closed_set.append(current)
+            children = current.children()
+            for child in children:
+                if not child in closed_set:
+                    temp_score = g_score[current] + self.distance_finder.distance(current, child)
+                    new_node = not child in open_set
+                    if new_node: open_set.append(child)
+                    if new_node or temp_score < g_score[child]:
+                        parents[child] = current
+                        g_score[child] = temp_score
+                        f_score[child] = g_score[child] + self.heuristic.estimate(self.distance_finder, child)
+
 class Node:
     matrix = file_to_matrix('../enunciado/adyacencia_grafo_dirigido_con_pesos.csv')
     tags = 'ABCDEFGHIJKL'
